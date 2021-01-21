603e6c89bd4846e073bd5282d1d58b468baeefa0
bishwajit.purkaystha <bishwajit.purkaystha@bracbank.com>
2021-01-22 01:03:21 +0600

    - React Table reverted to v6 - Few CSS for react table modified - User react table added (primarily, will require massive change later on) - pagination not done

--- src/component/User.js
+++ src/component/User.js
@@ -2,13 +2,16 @@ import React, {Component} from 'react'
 import Http from '../service/Http'
 import NotificationManager from 'react-notifications/lib/NotificationManager'
 import NotificationContainer from 'react-notifications/lib/NotificationContainer'
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
+import Title from './Title'
 
 class User extends Component {
 
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
-            users: '',
+            users: [],
             filter: {
                 filterName: '',
                 filterEmail: '',
@@ -46,7 +49,12 @@ class User extends Component {
 
         Http.GET('get_users', params)
             .then((response) => {
+
+                console.log('response: ', JSON.stringify(response, null, 2))
                 NotificationManager.success('Received users list from server')
+                this.setState({
+                    users: response.data.userList
+                })
                 console.log('response: ', JSON.stringify(response))
             })
             .catch((error) => {
@@ -75,11 +83,14 @@ class User extends Component {
 
         return (
             <div>
+
+                <Title value="Users"></Title>
+
                 <div className="row">
 
                     <form className="filtering-form row" onSubmit={this.filterBusinessRules}>
 
-                        <div className="col-md-3">
+                        <div className="col-md-3 offset-md-1">
 
                             <label htmlFor="filterName">NAME</label>
                             <input className="form-control" name="filterName"
@@ -94,7 +105,7 @@ class User extends Component {
                                 onChange={this.handleFilteringChange}/>
                         </div>
 
-                        <div className="col-md-3">
+                        <div className="col-md-2">
                             <label htmlFor="filterRole">ROLE</label>
                             <select value={this.state.filter.filterRole}
                                 name="filterRole" id="filterRole"
@@ -114,6 +125,54 @@ class User extends Component {
                     </form>
                 </div>
 
+                <div className="row center-content">
+
+                    <ReactTable
+                        data={this.state.users}
+                        columns = {
+                            [
+                                {
+                                    Header: 'S/N',
+                                    id: 'row',
+                                    filterable: false,
+                                    Cell: (row) => {
+                                        return <div> {row.index + 1} </div>
+                                    },
+                                    width: 60,
+                                    resizable: false,
+                                    style: {
+                                        textAlign: 'right'
+                                    }
+                                },
+                                {
+                                    Header: 'NAME',
+                                    width: 280,
+                                    accessor: 'name',
+                                    resizable: false,
+                                },
+                                {
+                                    Header: 'EMAIL',
+                                    width: 280,
+                                    accessor: 'email',
+                                    resizable: false,
+                                },
+                                {
+                                    Header: 'ROLE',
+                                    accessor: 'role',
+                                    resizable: false
+                                },
+                                {
+                                    Header: 'STATUS',
+                                    width: 180,
+                                    accessor: 'status',
+                                    resizable: false
+                                }
+                            ]
+                        }
+                        minRows = '2'
+                    />
+
+                </div>
                 <NotificationContainer/>
             </div>
         )
